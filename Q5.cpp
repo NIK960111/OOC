401f35584f203ff61f867a25c07201d0f896215e
NIK960111 <nasreenkulkarni04@gmail.com>
2026-08-10 09:54:29 +0530

    commit

---
+++ Q5.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int r,circumference,Area;
+
+cout<<"Enter the radius=";
+cin>>r;
+Area=3.14*r*r;
+circumference=2*3.14*r;
+cout<<"Area of circle="<<Area<<endl;
+cout<<"circumference of circle="<<circumference<<endl;
+
+
+return 0;
+
+}
